1f5587286fd560ff1dcb4ed468a87c353b0cf2b2
Niko_Laptop <jousjarvi@gmail.com>
2019-03-26 13:39:44 +0200

    front end added

---
+++ build/precache-manifest.ead1e48858fdca96b8dd93d32d23531c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fd0610df7bca7f0ad58b",
+    "url": "/static/js/main.a554de94.chunk.js"
+  },
+  {
+    "revision": "a0ffcc1f6d31788f2dfa",
+    "url": "/static/js/2.20c3c55f.chunk.js"
+  },
+  {
+    "revision": "d176486843d1ec0e6af170fdcc5d0ddc",
+    "url": "/index.html"
+  }
+];
